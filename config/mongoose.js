a06ebf796f75ad40933e41149fd883f3e11c75dd
rasmikanta <rasmikantabehera356@gmail.com>
2023-08-11 20:03:14 +0530

    db change

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/Placement-Cell');
+// mongoose.connect('mongodb://127.0.0.1:27017/Placement-Cell');
+mongoose.connect('mongodb+srv://rasmikanta:1234@cluster0.zrlrt7o.mongodb.net/?retryWrites=true&w=majority');
 // mongoose.connect('mongodb+srv://aninda:aninda123@cluster0.oq73w.mongodb.net/?retryWrites=true&w=majority');
+
 
 
 const db = mongoose.connection;
